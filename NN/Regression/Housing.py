e4a32c41589f27b9d26dee149779b09ba6664939
Santhosh Kumar Katta <kattasanthoshk@gmail.com>
2019-02-24 15:59:12 -0600

    Keras model using LR, MSE and Adam
    Keras model using LR, MSE and Adam

--- NN/Regression/Housing.py
+++ NN/Regression/Housing.py
@@ -13,3 +13,46 @@ y = dataset[:,13]
 print(x.shape)
 print(y.shape)
 
+def base_model():
+    model = Sequential()
+    #kernel_initializer --> Initial random varibales
+    model.add(Dense(13, activation='relu', input_dim=13, name='input', kernel_initializer='normal'))
+    # model.add(Dense(12, activation='relu', name='hidden1'))
+    model.add(Dense(1, name='output', kernel_initializer='normal'))
+    model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
+    return model
+
+np.random.seed(7)
+model = base_model()
+# train - Fit
+model.fit(x,y, epochs=500, batch_size=100, verbose=1, shuffle=False)
+
+# get matrx - Evaluate
+metrics = model.evaluate(x,y)
+print('acc: ',metrics[1]*100, '%')
+
+#predict
+print('x=', x[1])
+print('y=', y[1])
+print(x[1])
+
+print("==============")
+
+prediction = model.predict(x[1].reshape(1,13))
+print(prediction)
+
+# #Build the model
+# model = base_model()
+# #Train the model
+# model.fit(x, y, epochs=300, batch_size=100, verbose=1, shuffle=False)
+#
+# # evaluate the model
+# scores = model.evaluate(x, y)
+# print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+#
+# #predict
+# print(x[1])
+# print(y[1])
+# print("==============")
+# prediction = model.predict(x[1].reshape(1,13))
+# print(prediction)
