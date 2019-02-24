dbf4aa7cfd62331af4fc04bc795e68deae2cc722
Santhosh Kumar Katta <kattasanthoshk@gmail.com>
2019-02-24 16:31:51 -0600

    Plot - Loss
    Plot - Loss

--- NN/Plotting/plot_hist.py
+++ NN/Plotting/plot_hist.py
@@ -23,7 +23,7 @@ hist = model.fit(x,y, validation_split=0.33, epochs=150,batch_size=10, verbose=0
 
 print(hist.history.keys())
 
-#plot
+#Plot - Accuracy
 plt.plot(hist.history['acc'])
 plt.plot(hist.history['val_acc'])
 plt.xlabel('epoch')
@@ -31,6 +31,19 @@ plt.ylabel('accuracy')
 plt.title('Accuracy - Test vs Train')
 plt.legend(['train','val'])
 plt.show()
+
+#plot - Loss
+plt.plot(hist.history['loss'])
+plt.plot(hist.history['val_loss'])
+plt.xlabel('epoch')
+plt.ylabel('loss')
+plt.title('Loss - Train vs Test')
+plt.legend(['Train', 'Test'])
+plt.show()
+
+print('Acc: ', model.evaluate(x,y)[1]*100,'%')
+
+
 
 
 
